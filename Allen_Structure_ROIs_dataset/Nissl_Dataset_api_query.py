9991ba4ffd6fd3e0f2263eb3653591cfff00c41d
Mohamad Hawchar <60289580+MohamadHawshar@users.noreply.github.com>
2022-08-16 13:54:36 -0400

    Update Nissl_Dataset_api_query.py

--- Allen_Structure_ROIs_dataset/Nissl_Dataset_api_query.py
+++ Allen_Structure_ROIs_dataset/Nissl_Dataset_api_query.py
@@ -1,4 +1,12 @@
-import urllib.request, json 
+import urllib.request, json
+# http://api.brain-map.org/api/v2/data/query.json?criteria=
+# model::SectionDataSet,
+# rma::criteria,
+# [failed$eqfalse],products[id$eq1],treatments[name$eqNISSL],
+# plane_of_section[name$eq%27coronal%27]
+# ,rma::options
+# [start_row$eq0]
+# [num_rows$eq1000]
 with urllib.request.urlopen("http://api.brain-map.org/api/v2/data/query.json?criteria=model::SectionDataSet,rma::criteria,[failed$eqfalse],products[id$eq1],treatments[name$eqNISSL],plane_of_section[name$eq%27coronal%27],rma::options[start_row$eq0][num_rows$eq1000]") as url:
     data = json.loads(url.read().decode())
     print(len(data['msg']))

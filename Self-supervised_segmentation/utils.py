37f68f13713b779511735aa6e7b7fc12e3765437
Mohamad Hawchar <mohamad_hawchar@hotmail.com>
2023-01-14 22:20:54 -0500

    Added threshholding and reconstruction of crops

--- Self-supervised_segmentation/utils.py
+++ Self-supervised_segmentation/utils.py
@@ -32,12 +32,13 @@ def execution_time(start_time, end_time):
     1- OTSU thresholding on the attention map * original image
     2- OTSU thresholding on the original image
 """
-def threshold(img , attention, output_directory):
+def threshold(img , attention, output_directory,save = True):
     # multipli img with average attention
     result = img * attention / np.max(attention)
     # save result
     fname = os.path.join(output_directory, "result.png")
-    plt.imsave(fname=fname, arr=result, format='png')
+    if save:
+        plt.imsave(fname=fname, arr=result, format='png')
     #convert resul to CV_8UC1
     result = result.astype(np.uint8)
     # apply OTSU thresholding to the average result with opencv
@@ -45,11 +46,13 @@ def threshold(img , attention, output_directory):
     ret2, th2 = cv2.threshold( np.array(img), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_TRIANGLE)
     fname = os.path.join(output_directory, "OTSU_th" + "_average.png")
     # save as black and white cmap
-    plt.imsave(fname=fname, arr=th, format='png', cmap='gray')
-    print(f"{fname} saved.")
-    fname = os.path.join(output_directory, "OTSU_th" + "_original.png")
-    plt.imsave(fname=fname, arr=th2, format='png', cmap='gray')
-    print(f"{fname} saved.")
+    if save:
+        plt.imsave(fname=fname, arr=th, format='png', cmap='gray')
+        print(f"{fname} saved.")
+        fname = os.path.join(output_directory, "OTSU_th" + "_original.png")
+        plt.imsave(fname=fname, arr=th2, format='png', cmap='gray')
+        print(f"{fname} saved.")
+    return th, th2
 
 """ Prepare the attention map for visualization """
 def compute_attention(attentions, query,w_featmap, h_featmap, patch_size):
